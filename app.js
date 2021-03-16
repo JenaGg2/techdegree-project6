f49b5043374227201d4cd09dc667fffbfb08cfa1
Jeena.G <jeenagrg24@gmail.com>
2021-03-16 21:37:23 +0000

    changes

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const startButton = document.querySelector(".btn-rest");
+const liveHeart = document.querySelectorAll(".tries img");
 
 let missed = 0;
+
+startButton.addEventListener('click', () => {
+
+}
+
+
+
+)
