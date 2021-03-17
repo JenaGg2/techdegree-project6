a7ad3e4399de187df504b12348d72875f8744b32
Jeena.G <jeenagrg24@gmail.com>
2021-03-17 15:22:08 +0000

    changes

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
+//Variables ......
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const startButton = document.querySelector(".btn-rest");
-const liveHeart = document.querySelectorAll(".tries img");
+
+const startButton = document.querySelector('.btn__reset');
+const overlay = document.getElementById('overlay');
+
 
 let missed = 0;
 
-startButton.addEventListener('click', () => {
-
-}
-
-
-
-)
+//Button EventListener/funcion .....
+startButton.addEventListener("click", () => {
+ overlay.style.display = "none";
+});
